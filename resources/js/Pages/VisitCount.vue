18d460783625a954c1c533969ff0f8daeca55674
Seth Sharp <sesharp@outlook.com>
2024-10-23 11:42:02 +1000

    visit count

---
+++ resources/js/Pages/VisitCount.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+let count = ref(0)
+
+Echo.channel('hello-world').listen('HelloWorld', (e) => {
+  count.value = count.value + 1
+})
+</script>
+
+<template>
+  <div>
+    The visit count is {{ count }}
+  </div>
+</template>
